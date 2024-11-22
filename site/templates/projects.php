bcfca25db89746d8970cd28cec8b8f4b870b7f69
lkssmnt <hello@lsiemoneit.de>
2024-11-22 15:17:53 +0100

    initial commit

--- site/templates/projects.php
+++ site/templates/projects.php
@@ -5,7 +5,13 @@
 <ul>
   <?php foreach ($page->children() as $projectPage): ?>
     <li>
-      <a href="<?= $projectPage->url() ?>"><?= $projectPage->title() ?></a>
+      <a href="<?= $projectPage->url() ?>">
+        <img src="<?= $projectPage->gallery()->first()->toFile()->url() ?>">
+      </a>
+
+      <a href="<?= $projectPage->url() ?>">
+        <?= $projectPage->title() ?> | <?= $projectPage->author() ?>
+      </a>
     </li>
   <?php endforeach ?>
 </ul>
--- site/templates/text.php
+++ site/templates/text.php
@@ -1,5 +1,7 @@
 <?php snippet("header") ?>
 
+<h3><?= $page->author() ?>, <?= $page->date()->toDate("F Y") ?></h3>
+
 <div class="text">
   <?= $page->text() ?>
 </div>
